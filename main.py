39a1db01eff69f90cf7d9035bd2dfd6449cf53fa
Crisel <crisel@Guabayas.local>
2023-02-03 20:03:05 -0600

    :poop: poop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from typing import Text, Optional 
+from pydantic import BaseModel
 
 #  Se crea la instancia de FastAPI
 app = FastAPI(
@@ -18,11 +20,48 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+Usuarios = []
 
 #  Routes
 @app.get("/")
 async def root():
     return {"message": "ola"}
+
+##Elemento de la base
+class Usuario(BaseModel):
+     ID: int
+     Permiso: str
+     Contraseña: int
+
+#Agregando un usuario a Usuarios  
+@app.post("/Add")
+async def Login_Add(usuario:Usuarios, contraseña:Usuarios, permiso:Usuarios):
+     Usuarios.append(usuario)
+     Usuarios.append(contraseña)
+     Usuarios.append(permiso)
+     return{"Usuario":Usuarios}
+
+
+app.get('/Usuarios/{Usuarios_id}')
+def get_post():
+    return "received"
+
+#Modificar usuarios
+@app.put("/Modify")
+async def Login_Mod(usuarop: Usuario, contraseña: Usuario, permiso: Usuario):
+     for index, Usuario in enumerate(Usuarios):
+         if(Usuario["Usuarios_ID"]==Usuario and contraseña["Usuarios_ID"]==contraseña):
+             Usuarios[index].update(Usuario)
+             return{"Usuario":Usuarios}
+
+#Borrar usuarios
+@app.delete("/Delete")
+async def Login_Del(usuario:Usuarios, contraseña:Usuarios, permiso:Usuarios):
+     for index, usuario in enumerate(Usuarios):
+         if(usuario["Usuario_ID"]==usuario and contraseña["Usuarios_ID"]==contraseña):
+             del Usuarios[index]
+             return{"Usuario":Usuarios}
+
 
 #  Entry point
 if __name__ == "__main__":
