4fe4bd328c25c5760115b1d07404774c96ec27e6
Crisel <crisel@Guabayas.domain.name>
2023-02-04 21:29:13 -0600

    :sparkles: CRUD

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Text, Optional 
 from pydantic import BaseModel
+from typing import List
+
 
 #  Se crea la instancia de FastAPI
 app = FastAPI(
@@ -20,47 +22,129 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-Usuarios = []
+Usuarios = [
+    { 
+        "id": 1,
+        "usuario": "admin1",
+        "permiso": "admin",
+        "contraseña": "admin123"
+    },
+    {
+        "id": 2,
+        "usuario": "admin2",
+        "permiso": "admin",
+        "contraseña": "admin123"
+    },
+    {
+        "id": 3,
+        "usuario": "admin3",
+        "permiso": "admin",
+        "contraseña": "admin123"
+    },
+    {
+        "id": 4,
+        "usuario": "admin4",
+        "permiso": "admin",
+        "contraseña": "admin123"
+    }
+] 
 
-#  Routes
-@app.get("/")
-async def root():
-    return {"message": "ola"}
 
 ##Elemento de la base
 class Usuario(BaseModel):
-     ID: int
-     Permiso: str
-     Contraseña: int
+     id: int
+     usuario: str
+     permiso: str 
+     contraseña: str
+
+
+##Usuario sin mostrar la contraseña
+class Usuario_return(BaseModel):
+     id: int
+     usuario: str
+     permiso: str 
+
 
 #Agregando un usuario a Usuarios  
-@app.post("/Add")
-async def Login_Add(usuario:Usuarios, contraseña:Usuarios, permiso:Usuarios):
-     Usuarios.append(usuario)
-     Usuarios.append(contraseña)
-     Usuarios.append(permiso)
-     return{"Usuario":Usuarios}
+@app.post(
+    path="/",
+    response_model=Usuario_return, 
+    summary="Agrega un usuario",
+    description="Agrega un usuario a la base de datos",
+    tags=["Usuarios"]
+    )
+async def Login_Add(
+    usuario:Usuario = Body(..., example={
+        "id": 1,
+        "usuario": "admin1",
+        "permiso": "admin",
+        "contraseña": "admin123"
+        }),
+    ):
+     Usuarios.append(usuario.dict())
+     return usuario
 
 
-app.get('/Usuarios/{Usuarios_id}')
-def get_post():
-    return "received"
+#Enlistando todos los usuarios  
+@app.get(
+    path="/",
+    response_model=List[Usuario_return], 
+    summary="Lista de usuarios",
+    description="Regresamos todos los usuarios en una lista",
+    tags=["Usuarios"]
+    )
+async def Login_get():
+     return Usuarios
 
-#Modificar usuarios
-@app.put("/Modify")
-async def Login_Mod(usuarop: Usuario, contraseña: Usuario, permiso: Usuario):
-     for index, Usuario in enumerate(Usuarios):
-         if(Usuario["Usuarios_ID"]==Usuario and contraseña["Usuarios_ID"]==contraseña):
-             Usuarios[index].update(Usuario)
-             return{"Usuario":Usuarios}
 
-#Borrar usuarios
-@app.delete("/Delete")
-async def Login_Del(usuario:Usuarios, contraseña:Usuarios, permiso:Usuarios):
-     for index, usuario in enumerate(Usuarios):
-         if(usuario["Usuario_ID"]==usuario and contraseña["Usuarios_ID"]==contraseña):
-             del Usuarios[index]
-             return{"Usuario":Usuarios}
+#Regresando un usuario específico según su id  
+@app.get(
+    path="/{usuario_id}", 
+    response_model=Usuario_return, 
+    summary="Usuario",
+    description="Regresamos un usuario específico según su id",
+    tags=["Usuarios"]
+    )
+async def Login_get_id(usuario_id: int):
+        for usuario in Usuarios:
+            if usuario["id"] == usuario_id:
+                return usuario
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+
+#Eliminando un usuario específico según su id
+@app.delete(
+    path="/{usuario_id}", 
+    response_model=Usuario_return, 
+    summary="Eliminando usuario",
+    description="Eliminamos un usuario específico según su id",
+    tags=["Usuarios"]
+    )
+async def Login_del(usuario_id: int):
+        for usuario in Usuarios:
+            if usuario["id"] == usuario_id:
+                Usuarios.remove(usuario)
+                return usuario
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+
+#Actualizando un usuario específico según su id    
+@app.put(
+    path="/{usuario_id}", 
+    response_model=Usuario_return, 
+    summary="Actualizar usuario",
+    description="Actualizamos un usuario específico según su id",
+    tags=["Usuarios"]
+    )
+async def Login_put(
+    usuario_id: int, new_usuario: Usuario):
+        for usuario in Usuarios:
+            if usuario["id"] == usuario_id:
+                usuario["usuario"] = new_usuario.usuario
+                usuario["contraseña"] = new_usuario.contraseña
+                usuario["permiso"] = new_usuario.permiso
+                return usuario
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
 
 #  Entry point
